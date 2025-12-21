2a123e0d27aeb8f5e071ae309b0cdbfa12fbe28b
X. Samuel Ji <samuel.j032@icloud.com>
2025-12-20 20:16:15 -0800

    feat: implement home route with pagination and data fetching

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,9 +1,22 @@
+import { paginationSchema } from "@shared/validators/search.validation";
+import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { postsInfiniteQueryOptions } from "@/services/posts";
 
 export const Route = createFileRoute("/")({
 	component: Index,
+	validateSearch: paginationSchema,
 });
 
 function Index() {
-	return <div>Hello world</div>;
+	const { page, limit, sortBy, order, author, site } = Route.useSearch();
+	const { data } = useSuspenseInfiniteQuery(
+		postsInfiniteQueryOptions({ page, limit, sortBy, order, author, site }),
+	);
+	return (
+		<div className="p-2">
+			<h3>Welcome Home!</h3>
+			{JSON.stringify(data)}
+		</div>
+	);
 }
