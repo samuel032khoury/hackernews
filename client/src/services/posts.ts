a7bec7672be41d42e109d4cd21d8639c05b07a38
X. Samuel Ji <samuel.j032@icloud.com>
2025-12-20 19:13:05 -0800

    feat: implement getAllPosts function with pagination and sorting

--- client/src/services/posts.ts
+++ client/src/services/posts.ts
@@ -1,5 +1,25 @@
 import type { ApiError } from "@shared/types";
+import type { paginationSchema } from "@shared/validators/search.validation";
+import type z from "zod";
 import { api } from "@/lib/api";
+
+export const getAllPosts = async ({
+	page = 1,
+	sortBy,
+	order,
+	author,
+	site,
+}: z.infer<typeof paginationSchema>) => {
+	const res = await api.posts.$get({
+		query: {
+			page: page.toString(),
+			sortBy: sortBy,
+			order: order,
+			author: author,
+			site: site,
+		},
+	});
+};
 
 export const getPost = async (id: number) => {
 	const res = await api.posts[":id"].$get({ param: { id: id.toString() } });
