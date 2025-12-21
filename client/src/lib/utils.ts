6ada63b9f1622e034b0bd8ae4dca9ea38824ab52
X. Samuel Ji <samuel.j032@icloud.com>
2025-12-20 23:39:20 -0800

    feat: enhance index route with SortBar and PostCard components

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { type ClassValue, clsx } from "clsx";
+import { DateTime } from "luxon";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
+}
+
+export function relativeTime(date: string) {
+	const datetime = DateTime.fromISO(date);
+	return datetime.toRelative();
 }
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { paginationSchema } from "@shared/validators/search.validation";
 import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { PostCard } from "@/components/post-card";
+import { SortBar } from "@/components/sort-bar";
 import { postsInfiniteQueryOptions } from "@/services/posts";
 
 export const Route = createFileRoute("/")({
@@ -10,13 +12,17 @@ export const Route = createFileRoute("/")({
 
 function Index() {
 	const { page, limit, sortBy, order, author, site } = Route.useSearch();
-	const { data } = useSuspenseInfiniteQuery(
-		postsInfiniteQueryOptions({ page, limit, sortBy, order, author, site }),
-	);
+	const { data, isFetchingNextPage, fetchNextPage, hasNextPage } =
+		useSuspenseInfiniteQuery(
+			postsInfiniteQueryOptions({ page, limit, sortBy, order, author, site }),
+		);
 	return (
-		<div className="p-2">
-			<h3>Welcome Home!</h3>
-			{JSON.stringify(data)}
+		<div className="mx-auto max-w-3xl p-4">
+			<h1 className="mb-6 font-bold text-2xl text-foreground">Submissions</h1>
+			<SortBar sortBy={sortBy} order={order} />
+			{data?.pages.map((page) =>
+				page.data.map((post) => <PostCard key={post.id} post={post} />),
+			)}
 		</div>
 	);
 }
