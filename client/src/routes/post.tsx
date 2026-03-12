dad7aa65680aaddf35c3af9c14e62bb20e3a25cc
X. Samuel Ji <samuel.j032@icloud.com>
2026-03-11 19:42:39 -0700

    feat: add post card on post detail page

--- client/src/routes/post.tsx
+++ client/src/routes/post.tsx
@@ -1,12 +1,21 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { pathSearchSchema } from "@/validators/path.validation";
+import { PostCard } from "@/components/post-card";
+import { postQueryOptions } from "@/services/posts";
+import { pathPostSchema } from "@/validators/path.validation";
 
 export const Route = createFileRoute("/post")({
 	component: Post,
-	validateSearch: pathSearchSchema,
+	validateSearch: pathPostSchema,
 });
 
 function Post() {
-	const { id, sortBy, order } = Route.useSearch();
-	return <div>Hello "/post"! {id}</div>;
+	const { id } = Route.useSearch();
+	// get data with suspense
+	const { data } = useSuspenseQuery(postQueryOptions(id));
+	return (
+		<div className="mx-auto max-w-3xl">
+			{data && <PostCard post={data.data} />}
+		</div>
+	);
 }
